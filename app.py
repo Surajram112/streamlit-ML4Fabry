19a7eba645f53d9d9ab76d96840ce49124d204be
Suraj Ramchand <866648@swansea.ac.uk>
2024-04-09 13:20:08 +0100

    s

--- app.py
+++ app.py
@@ -323,7 +323,7 @@ with pred_cont.container():
   
   with st.container():
     # Create a SHAP Explainer object
-    shap_values = model.get_booster().predict(xgb.DMatrix(input_data), pred_contribs=True)
+    shap_values = model.get_booster().predict(xgb.DMatrix(input_data), pred_contribs=True)[:,:-1]
     
     # Create a SHAP summary plot
     fig, ax = plt.subplots()
