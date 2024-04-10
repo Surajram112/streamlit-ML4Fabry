0b54e8efc11bfed3c2f4ac64cd01fd24f20238e7
Suraj Ramchand <866648@swansea.ac.uk>
2024-04-10 17:01:45 +0100

    i

--- app.py
+++ app.py
@@ -357,48 +357,48 @@ with pred_cont.container():
         y=altair.Y('Feature:N', sort='-x')
     ), use_container_width=True)
 
-with st.sidebar:
-  st.title('🤗💬 Ask Away!')
-  st.caption("🚀 A streamlit chatbot powered by a HuggingFace LLM")
-  
-  if 'generated' not in st.session_state:
-    st.session_state['generated'] = ["I'm HugChat, How may I help you?"]
+st.title('🤗💬 Ask Away!')
+st.caption("🚀 A streamlit chatbot powered by a HuggingFace LLM")
 
-  if 'past' not in st.session_state:
-      st.session_state['past'] = ['Hi!']
+if 'generated' not in st.session_state:
+  st.session_state['generated'] = ["I'm HugChat, How may I help you?"]
+
+if 'past' not in st.session_state:
+    st.session_state['past'] = ['Hi!']
+    
+input_container = st.container()
+colored_header(label='', description='', color_name='blue-30')
+response_container = st.container()
+
+# User input
+## Function for taking user provided prompt as input
+def get_text():
+    input_text = st.text_input("You: ", "", key="input")
+    return input_text
+
+## Applying the user input box
+with input_container:
+    user_input = get_text()
+
+# Response output
+## Function for taking user prompt as input followed by producing AI generated responses
+def generate_response(prompt):
+    chatbot = hugchat.ChatBot()
+    response = chatbot.chat(prompt)
+    return response
+
+with response_container:
+  if user_input:
+      response = generate_response(user_input)
+      st.session_state.past.append(user_input)
+      st.session_state.generated.append(response)
       
-  input_container = st.container()
-  colored_header(label='', description='', color_name='blue-30')
-  response_container = st.container()
-  
-  # User input
-  ## Function for taking user provided prompt as input
-  def get_text():
-      input_text = st.text_input("You: ", "", key="input")
-      return input_text
+  if st.session_state['generated']:
+      for i in range(len(st.session_state['generated'])):
+          message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+          message(st.session_state['generated'][i], key=str(i))
+          
 
-  ## Applying the user input box
-  with input_container:
-      user_input = get_text()
-  
-  # Response output
-  ## Function for taking user prompt as input followed by producing AI generated responses
-  def generate_response(prompt):
-      chatbot = hugchat.ChatBot()
-      response = chatbot.chat(prompt)
-      return response
-  
-  with response_container:
-    if user_input:
-        response = generate_response(user_input)
-        st.session_state.past.append(user_input)
-        st.session_state.generated.append(response)
-        
-    if st.session_state['generated']:
-        for i in range(len(st.session_state['generated'])):
-            message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
-            message(st.session_state['generated'][i], key=str(i))
-  
   
   # with st.form('LLM_Chatbot'):
   #   input_text = st.text_area('Enter text:', 'Do you have any questions about the patient or explanation?')
