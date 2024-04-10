511337d110068afd25de5128abaac449210ae5cb
Suraj Ramchand <866648@swansea.ac.uk>
2024-04-10 17:57:45 +0100

    s

--- app.py
+++ app.py
@@ -426,7 +426,6 @@ ai.write(explanation["text"])
 
 # Chat input widget
 user_input = st.chat_input("Tell me any questions you have or if you need further insight into the patient explanation!")
-human.write(user_input)
 
 # Response output
 if user_input:
@@ -434,6 +433,8 @@ if user_input:
     response = chatbot.query(user_input, web_search=True)
     st.session_state.past.append(user_input)
     st.session_state.generated.append(response)
+    
+    human.write(user_input)
     ai.write(response)
 
 # # Response output
