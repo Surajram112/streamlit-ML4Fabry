77befa54f3ed789cdbfa3309e1bf8bdd7a9881fe
Suraj Ramchand <866648@swansea.ac.uk>
2024-04-11 19:07:26 +0100

    s

--- app.py
+++ app.py
@@ -319,7 +319,8 @@ with pred_cont.container():
     # Base chart for the single bar
     base = alt.Chart(data).mark_bar().encode(
         x=alt.X('Probability:Q', stack='zero', axis=None),
-        color=alt.Color('Condition:N', legend=None, scale=alt.Scale(domain=['HCM', 'FD'], range=['#1f77b4', '#ff7f0e']))
+        color=alt.Color('Condition:N', legend=None, scale=alt.Scale(domain=['HCM', 'FD'], range=['#1f77b4', '#ff7f0e'])),
+        text=alt.Text('Probability:Q', format='.2f')
         ).properties(
             height=50
         )
