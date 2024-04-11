ea8622d8d0294e0311421e7b2fad2c9dfefd37e4
Suraj Ramchand <866648@swansea.ac.uk>
2024-04-11 15:15:41 +0100

    s

--- app.py
+++ app.py
@@ -412,10 +412,8 @@ llm = HuggingFaceEndpoint(
 
 model_instructions = PromptTemplate.from_template(template)
 llm_chain = LLMChain(llm=llm, prompt=model_instructions)
-
-if "messages" not in st.session_state:
-    response = llm_chain.invoke(prompt)
-    st.session_state["messages"] = [ChatMessage(role="assistant", content=response.content)]
+response = llm_chain.invoke(prompt)
+st.session_state["messages"] = [ChatMessage(role="assistant", content=response.content)]
 
 for msg in st.session_state.messages:
     st.chat_message(msg.role).markdown(msg.content)
