2b204bc0599d6444891f473fa28ac69b5044edfb
Suraj Ramchand <866648@swansea.ac.uk>
2024-04-11 21:40:28 +0100

    s

--- app.py
+++ app.py
@@ -399,7 +399,7 @@ with pred_cont.container():
     ), use_container_width=True)
 
 # Create a line between the containers and the chatbot
-colored_header(label='', description='', color_name='blue-green-70')
+colored_header(label='', description='', color_name='light-blue-70')
 
 # # ChatBot
 # st.title('🤗💬 Ask Away!')
