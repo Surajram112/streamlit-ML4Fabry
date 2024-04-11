5ee4dc5cb1c4c5850818a845c6dbdcff9a398ae7
Suraj Ramchand <866648@swansea.ac.uk>
2024-04-11 22:25:17 +0100

    u

--- app.py
+++ app.py
@@ -421,7 +421,7 @@ stream_handler = StreamHandler(st.empty())
 
 # Initialize the LLM model
 llm = HuggingFaceEndpoint(
-        repo_id="mistralai/Mixtral-8x7B-Instruct-v0.1",
+        repo_id=st.secrets["HUGGINGFACE_REPO_ID"],
         task="text-generation",
         max_new_tokens=2048,
         top_k=10,
