abeabd7a65678c34ee2e8f094dff1a255a51f441
Suraj Ramchand <866648@swansea.ac.uk>
2024-04-11 12:31:17 +0100

    j

--- app.py
+++ app.py
@@ -44,8 +44,7 @@ cookies = sign.login(cookie_dir_path=cookie_path_dir, save_cookies=True)
 
 # Create your ChatBot
 chatbot = hg.ChatBot(cookies=cookies.get_dict())
-id = chatbot.new_conversation()
-chatbot.change_conversation(id)
+chatbot.new_conversation(switch_to=True, assistant='zephyr-7b-alpha')
 
 # Set today's date to ensure all reports are on or before this date
 today = datetime.today().date()
