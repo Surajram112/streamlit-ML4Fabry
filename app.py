6ae908b3669fa47a0ec3cf5e4154af6a473760a4
Suraj Ramchand <866648@swansea.ac.uk>
2024-04-11 13:41:30 +0100

    s

--- app.py
+++ app.py
@@ -401,7 +401,7 @@ msgs = StreamlitChatMessageHistory()
 memory = ConversationBufferMemory(
     chat_memory=msgs, return_messages=True, memory_key="chat_history", output_key="output"
 )
-if len(msgs.messages) == 0 or st.sidebar.button("Reset chat history"):
+if len(msgs.messages) == 0 or st.button("Reset chat history"):
     msgs.clear()
     msgs.add_ai_message("How can I help you?")
     st.session_state.steps = {}
