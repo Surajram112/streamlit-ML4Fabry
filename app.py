4490cd4ff245342423b31cc105eca6d6ea447e03
Suraj Ramchand <866648@swansea.ac.uk>
2024-04-12 07:43:11 +0100

    s

--- app.py
+++ app.py
@@ -460,7 +460,7 @@ if 'initialized' not in st.session_state:
 
 # Initialize the LLM model
 llm = HuggingFaceEndpoint(
-    repo_id="mistralai/Mistral-7B-Instruct-v0.1",
+    repo_id=st.secrets["HUGGINGFACE_REPO_ID"],
     task="text-generation",
     max_new_tokens=100,
     huggingfacehub_api_token=st.secrets["HUGGINGFACEHUB_API_TOKEN"]
